32d2af180e616090439238cf713fc9eb8eaa48a3
Stephen McMillan <stephen1192@hotmail.co.uk>
2019-02-22 15:47:35 +0000

    Initial Commit

---
+++ Proximity Reminders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proximity Reminders
+//
+//  Created by Stephen McMillan on 22/02/2019.
+//  Copyright © 2019 Stephen McMillan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
